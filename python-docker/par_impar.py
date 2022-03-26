a76c2005f08a723988824fccec48f3695a8e3ccc
Deivid Duarte <deividdua@gmail.com>
2022-03-26 01:14:50 -0300

    Corrigido erro de valor inválido

--- python-docker/par_impar.py
+++ python-docker/par_impar.py
@@ -1,6 +1,11 @@
-num=int(input("Digite um número: "))
+num = input('Digite um número: ')
 
-if num % 2 == 0:
-  print(f'O número {num} é par')
+
+if num.isnumeric():
+  num = int(num)
+  if num % 2 == 0:
+    print(f'O número {num} e par')
+  if num % 2 != 0:
+    print(f'O número {num} é impar')
 else:
-  print(f'O número {num} é impar')
+  print('Valor invalido')
